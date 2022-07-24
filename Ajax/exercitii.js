ac2e1f2379cb837141df9504291fe6fb7d96062d
Ion Mariana <marianaion319@gmail.com>
2022-07-24 20:38:57 +0300

    adding more files in ajax

---
+++ Ajax/exercitii.js
@@ -0,0 +1,133 @@
+// var printOdd = (array) => {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 !== 0) {
+//             console.log(array[i]);
+//         }
+//     }
+// };
+// var printEven = (array) => {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 === 0) {
+//             console.log(array[i]);
+//         }
+//     }
+// };
+// printOdd([1, 2, 3, 4, 5]);
+// printEven([1, 2, 3, 4, 5]);
+
+// var printNumbers = (array) => {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 !== 0) {
+//             console.log(array[i]);
+//         }
+//     }
+// };
+
+
+// printNumbers([1, 2, 3, 4, 5]);
+
+
+
+// function sumOfDigits(number){
+//     let sum= 0;
+//     while(number !==0){
+//         if (evenNumber(number%10)){
+//         sum += number % 10;
+//         }
+        
+//         number = Math.floor(number/= 10);
+//     }
+//         return sum;
+// }
+
+// //exemplu 
+// let num =146; 
+// let rezultat = sumOfDigits(num);
+
+
+// sa determinam cati divizori pari are un numar 
+
+//numar= 6 se va afisa numarul 2 pentru ca are 2 si 6 ca divizori
+
+
+// function numberOfDivisions(number){
+//     let counter = 0;
+//     for (var i=1; i<=number; i++){
+//         if(number%i == 0 && i%2 == 0){
+//             counter ++;
+//         }
+//     }
+//         return counter
+// }
+
+// numberOfDivisions(6);
+// console.log(numberOfDivisions(6));
+
+
+
+// function numberOfDivisions(number){
+//     let counter = 0;
+//     for (var i=1; i<=number; i+=2){
+//         if(number%i == 0 && i%2 == 0){
+//             counter ++;
+//         }
+//     }
+//         return counter
+// }
+
+// numberOfDivisions(6);
+// console.log(numberOfDivisions(6));
+
+let user1= {
+    age:19,
+    firstName:"Maria",
+    lastName:"Ion"
+};
+
+let yser2= {
+    age: 37,
+    firstName:"Andrada",
+    lastName: "Voinea"
+};
+
+console.log(user1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
