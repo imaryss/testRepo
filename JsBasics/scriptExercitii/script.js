f6e021dfed9ee97f1af63bb405363399c3a73c55
Ion Mariana <marianaion319@gmail.com>
2022-07-25 23:29:23 +0300

    new files

---
+++ JsBasics/scriptExercitii/script.js
@@ -0,0 +1,386 @@
+//*HTML
+//?(1)
+//Creati o pagina noua html, cu denumirea in tab-ul ferestreti de browser de Ex1
+//Puneti Textul de Seasons pe prima linie de pe ecran
+//Puneti 4 cuvinte pe acelasi rand : Spring, Summer, Autumn si Winter
+//Spring trebuie sa fie cu bold, Summer subliniat, Autumn italic si Winter bold, italic si subliniat
+//Culoare distincta pe fiecare anotimp in parte
+//Make each word to be clickable and when clicked a new tab should be opened to a picture of the seasons
+//?(2)
+//Using Windows+. open the emoji window and pick you favorite emoji
+//Using HTML special characters put on the screen 1 emoji on the first row, on the second row
+//two emojis, on the thirs tree emojis ..... until the seventh with seven emoji
+//Make sure that all the emoji are aligned to the right
+//?(3)
+
+//Create a new folder called HomeworkHTML with an index.html file
+//Inside the folder HomeworkHTML create another folder called img where you will store all the images for this homework
+//Put the title "HomeworkIsFun" for your html file ... it should appear in the tab of the browser
+// Learn from googling how you can put a small image on the left of the title of the tab, just like in the case of Slack or a tab from the Academy with the slides (it is a very small image there)
+//Create 3 paragraphs. Put them all inside a div element
+// The first two paragraphs should have the text inside (use blind text generator for the content of the paragraphs) of color red
+//Make the first word of every paragraph bold, italic and underlined
+//Make the last work of each paragraph a link
+//Open the first link in a new tab in the browser, the other links should open in the same tab
+// Color the links in different colors
+//Create an unordered list with items. Each should be a different heading from 1 to 6, in different colors
+//Create an ordered list about the Marvel Movies. Make each item contain a wallpaper of the movie and the title of the move (try to find a very small image so it would fit on the same row as the title)
+//Create a table with favorite countries in the world. You should have information in the table about : capital, population, surface, languages. It is up to you to create the design for the table
+//Create a rhomboid shape with emojis while using HTML special characters
+//Create a new html file. This file will contain only on picture. When you click on it, the picture will be replaced with another picture. How do you do it?
+//All the items with asterisc are more difficult - do not stress if you can't solve them easily
+//*CSS
+//?(4)
+//Create a form with:
+//2 textfields with default values
+//1 textarea with default value for feedback for the product
+//3 radio buttons for favorite meal
+// email input with any kind of property that you know
+//color input with default value .... yellow
+//3 checkboxes
+//submit button
+//?(5)
+//Create 3 divs, each with 4 paragraphs each
+//	Inside the first paragraph for each div you will have at least 2 links and 1 span
+// Put the same class for the first and third paragraph, and another class for the second and fourth paragraph and style them with background-colors : chooses nuances of light grey
+//  Both classes should have the following set of rules : triple the size of the text inside and same font family
+// Target the span inside the second and forth paragraph only and add some tet decoration properties, and make the letter all uppercase
+// Create a span outside the paragraphs, but still inside the divs. If you want to put a background-color property only for the spans inside the div, but outside the paragraphs ... how do you do it? Forbidden the usage of classes or ids
+// Put an image. When you stay with your mouse over the image - it should gain : border, margin and padding; try to put margin, border and padding only on bottom;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//*JS
+//Exercise* : marius.monea89@gmail.com
+//?(1)
+//You are given this string
+// please create an algorithm that will parse the string and separate the first name and the last name into variables
+// also .... the extension should be in a variable as well
+// in the end you should have something like marius, monea89 si gmail in separate variables
+//?(2)
+//Exercise if else : create a variable dayOfTheWeek. Put a string value inside; check with if else what is the day of the week and put  a different message for each day
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
