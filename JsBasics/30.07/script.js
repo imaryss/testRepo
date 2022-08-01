4a19ae48eefa2665ceef0e84615a01f76bcf306c
Ion Mariana <marianaion319@gmail.com>
2022-08-01 13:14:54 +0300

    curs 30/07- hangman and rainbow

--- JsBasics/30.07/script.js
+++ JsBasics/30.07/script.js
@@ -1,1 +1,39 @@
-//Exercise1: Create a Rainbow
+//curs udemy
+//Selecting elements
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+// document.querySelector('.header')
+
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+// let circleClass=document.getElementsByClassName('circle');
+// console.log(circleClass)
+
+// life collection= buttons= if the dom 
+//changes the life collection will automacally be updated
+//ex: stergi un element din pagina=> array ul cu butoane din 
+//consola iti va sterge acel buton automat
+
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.textContent ='Adding some text'
+// message.innerHTML='Adding some text'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- JsBasics/30.07/script2.js
+++ JsBasics/30.07/script2.js
@@ -1,1 +1,53 @@
-prompt('Introduce a letter')
+// Set de exercitii alternative pentru cine doreste :
+//* Se da un cuvant . Numarati de cate ori se repeta fiecare litera.
+let  letterString= "MARIANAMARIANAMARIANA";
+let splitedString = letterString.split('');
+console.log(splitedString);
+console.log(splitedString.valueOf());
+const count = {};
+
+
+splitedString.forEach(letter => {
+    console.log(letter + '9');
+});
+// for(var i=0; i<letterString.length; i++){
+
+// }
+// numOfrepeatedLetters
+// 2. Se da un paragraf cu mai mai multe propozitii. 
+// Sa se calculeze litera care se repeta de cele mai multe ori, 
+// indiferent daca este lowercase sau uppercase
+// 3. Sa se scrie o regula REGEX care sa selecteze numerele de telefon romanesti
+// 4. Se da un cuvant. Creati un alt cuvant care sa contina doar vocalele din primul cuvant
+// 5. Creati o clasa de obiecte Student. 
+// Fiecare student are un nume si 3 note la materiile:
+//  Matematica, Romana, Informatica. Instantiati de 4 ori clasa Student
+// Introducti o noua metoda pe clasa Student care sa afiseze media generala
+// 6. Calculati cea mai mare nota la Informatica din obiectele create
+// 7. Creati 5 cercuri ca la jocurile olimpice. Colorati-le background-ul
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*********************REVIN IN 2 MINUTE********************
+
+
+
+
+
+
+
+
