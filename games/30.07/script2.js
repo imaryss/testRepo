d5dd1eced57fa0f166aa94523806978c292eb05b
Ion Mariana <marianaion319@gmail.com>
2022-08-08 20:42:56 +0300

    hangman

--- games/30.07/script2.js
+++ games/30.07/script2.js
@@ -1,113 +1,157 @@
-// Set de exercitii alternative pentru cine doreste :
-//* Se da un cuvant . Numarati de cate ori se repeta fiecare litera.
-let  letterString= "MARIANAMARIANAMARIANA";
-let splitedString = letterString.split('');
-console.log(splitedString);
-console.log(splitedString.valueOf());
-const count = {};
-console.log(letterString.length)
-// for(let i=0; i<=splitedString.length; i++){
-//     if(splitedString[i] === 'M' ){
-//     console.log(splitedString[i]);
-//     }
-// }
-// for(let i=0; i<=splitedString.length; i++){
-//     if(splitedString[i] === 'A' ){
-//     console.log(splitedString[i]);
-//     }
-// }
+const letters = document.querySelectorAll(".alphabet");
 
-// for(let i=0; i<=splitedString.length; i++){
-//     if(splitedString[i] === 'R' ){
-//     console.log(splitedString[i]);
-//     }
-// }
-
-// for(let i=0; i<=splitedString.length; i++){
-//     if(splitedString[i] === 'I' ){
-//     console.log(splitedString[i]);
-//     }
-// }
-
-// for(let i=0; i<=splitedString.length; i++){
-//     if(splitedString[i] === 'A' ){
-//     console.log(splitedString[i]);
-//     }
-// }
-
-// for(let i=0; i<=splitedString.length; i++){
-//     if(splitedString[i] === 'N' ){
-//     console.log(splitedString[i]);
-//     }
-// }
-
-// for(let i=0; i<=splitedString.length; i++){
-//     if(splitedString[i] === 'A' ){
-//     console.log(splitedString[i]);
-//     }
-// }
-
-splitedString.forEach(function(){
-
+const guessWord = [
+  "rainbow",
+  "sunshine",
+  "absolute",
+  "across",
+  "adapt",
+  "address",
+  "adjust",
+  "become",
+  "black",
+  "central",
+  "degree",
+  "detailed",
+  "emergency",
+  "enter",
+  "fate",
+];
+let randomWord =
+  guessWord[Math.floor(Math.random() * guessWord.length - 1) + 1];
+console.log(randomWord);
+let filledString = "";
+let symbol = "#";
+for (let j = 0; j <= randomWord.length - 1; j++) {
+  filledString += symbol;
 }
-);
-// for(var i=0; i<letterString.length; i++){
 
+let idA = document.querySelector("#A");
+let idB = document.querySelector("#B");
+let idC = document.querySelector("#C");
+let idD = document.querySelector("#D");
+let idE = document.querySelector("#E");
+let idF = document.querySelector("#F");
+let idG = document.querySelector("#G");
+let idH = document.querySelector("#H");
+let idI = document.querySelector("#I");
+let idJ = document.querySelector("#J");
+let idK = document.querySelector("#K");
+let idL = document.querySelector("#L");
+let idM = document.querySelector("#M");
+let idN = document.querySelector("#N");
+let idO = document.querySelector("#O");
+let idP = document.querySelector("#P");
+let idQ = document.querySelector("#Q");
+let idR = document.querySelector("#R");
+let idS = document.querySelector("#S");
+let idT = document.querySelector("#T");
+let idW = document.querySelector("#W");
+let idX = document.querySelector("#X");
+let idY = document.querySelector("#Y");
+let idZ = document.querySelector("#Z");
+
+idA.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+
+idB.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idC.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idD.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idE.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idF.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idG.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idH.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idI.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idJ.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idK.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idL.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idM.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idN.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idO.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idP.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idQ.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idR.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idS.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idT.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idW.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idX.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idY.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+idZ.addEventListener("click", function (event) {
+  console.log(event.target.textContent);
+});
+let word4 = "";
+letters.forEach((element) => {
+  element.addEventListener("click", function () {
+    for (var i = 0; i <= randomWord.length - 1; i++) {
+      if (element.innerHTML.toLowerCase() == randomWord[i]) {
+        word4 += randomWord[i];
+      } else if (element.innerHTML.toLowerCase() !== randomWord[i]) {
+        word4 += "#";
+        randomWord[i] = "#";
+        console.log("else if");
+      } else {
+        // randomWord[i] = "#";
+        console.log("else");
+      }
+    }
+    console.log(word4);
+  });
+});
+// else if randomword[i]="#"
+// const word1 = "sunshine";
+// let word2 = "########";
+// let word3 = "";
+// let letter = "n";
+// for (var i = 0; i <= word1.length - 1; i++) {
+//   word1[i] == letter ? (word3 = word3 + word1[i]) : (word3 = word3 + "#");
 // }
-// numOfrepeatedLetters
-// 2. Se da un paragraf cu mai mai multe propozitii. 
-// Sa se calculeze litera care se repeta de cele mai multe ori, 
-// indiferent daca este lowercase sau uppercase
-// 3. Sa se scrie o regula REGEX care sa selecteze numerele de telefon romanesti
-// 4. Se da un cuvant. Creati un alt cuvant care sa contina doar vocalele din primul cuvant
-// 5. Creati o clasa de obiecte Student. 
-// Fiecare student are un nume si 3 note la materiile:
-//  Matematica, Romana, Informatica. Instantiati de 4 ori clasa Student
-// Introducti o noua metoda pe clasa Student care sa afiseze media generala
-// 6. Calculati cea mai mare nota la Informatica din obiectele create
-// 7. Creati 5 cercuri ca la jocurile olimpice. Colorati-le background-ul
+// console.log(word3);
+// word2 = word3;
+// console.log(word2);
 
-// document.querySelector('button').addEventListener(
-//     'click', function() {
-//         console.log("i clicked it")
-//     }
-// )
-
-// document.querySelectorAll(".alphabet").addEventListener(
-//     'click', function(){
-//         console.log("click");
-//     }
-// )
-
-// const letters = document.querySelectorAll(".alphabet");
-// console.log(letters);
-
-// for(let i=0; i<letters.length; i++){
-//     letters[i].addEventListener(
-//         'click', function(){
-//             console.log("click");
-//         }
-//     )
-// }
-
-// letters.forEach(element => {
-//     element.addEventListener(
-//         'click', function(){
-//             console.log('click');
-//         }
-//     )
-// });
-
-
-
-
-
-//*********************REVIN IN 2 MINUTE********************
-
-
-
-
-
-
-
-
+//*----------------------REVIN IN 2 MINUTE---------------------------
---
+++ games/30.07/suplimentar.js
@@ -0,0 +1,72 @@
+// *2. Se da un paragraf cu mai mai multe propozitii.
+// *Sa se calculeze litera care se repeta de cele mai multe
+// *ori, indiferent daca este lowercase sau uppercase
+const paragraph =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+//*********************REVIN IN 2 MINUTE********************
+// Set de exercitii alternative pentru cine doreste :
+//* Se da un cuvant . Numarati de cate ori se repeta fiecare litera.
+// let letterString = "MARIANAMARIANAMARIANA";
+// let splitedString = letterString.split("");
+// console.log(splitedString);
+// console.log(splitedString.valueOf());
+// const count = {};
+// console.log(letterString.length);
+// for(let i=0; i<=splitedString.length; i++){
+//     if(splitedString[i] === 'M' ){
+//     console.log(splitedString[i]);
+//     }
+// }
+// for(let i=0; i<=splitedString.length; i++){
+//     if(splitedString[i] === 'A' ){
+//     console.log(splitedString[i]);
+//     }
+// }
+
+// for(let i=0; i<=splitedString.length; i++){
+//     if(splitedString[i] === 'R' ){
+//     console.log(splitedString[i]);
+//     }
+// }
+
+// for(let i=0; i<=splitedString.length; i++){
+//     if(splitedString[i] === 'I' ){
+//     console.log(splitedString[i]);
+//     }
+// }
+
+// for(let i=0; i<=splitedString.length; i++){
+//     if(splitedString[i] === 'A' ){
+//     console.log(splitedString[i]);
+//     }
+// }
+
+// for(let i=0; i<=splitedString.length; i++){
+//     if(splitedString[i] === 'N' ){
+//     console.log(splitedString[i]);
+//     }
+// }
+
+// for(let i=0; i<=splitedString.length; i++){
+//     if(splitedString[i] === 'A' ){
+//     console.log(splitedString[i]);
+//     }
+// }
+
+// splitedString.forEach(function () {});
+// for(var i=0; i<letterString.length; i++){
+
+// }
+// numOfrepeatedLetters
+// 2. Se da un paragraf cu mai mai multe propozitii.
+// Sa se calculeze litera care se repeta de cele mai multe ori,
+// indiferent daca este lowercase sau uppercase
+// 3. Sa se scrie o regula REGEX care sa selecteze numerele de telefon romanesti
+// 4. Se da un cuvant. Creati un alt cuvant care sa contina doar vocalele din primul cuvant
+// 5. Creati o clasa de obiecte Student.
+// Fiecare student are un nume si 3 note la materiile:
+//  Matematica, Romana, Informatica. Instantiati de 4 ori clasa Student
+// Introducti o noua metoda pe clasa Student care sa afiseze media generala
+// 6. Calculati cea mai mare nota la Informatica din obiectele create
+// 7. Creati 5 cercuri ca la jocurile olimpice. Colorati-le background-ul
